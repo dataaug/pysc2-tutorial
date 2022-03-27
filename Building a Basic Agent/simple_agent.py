1e3ad7800037dddf1ed1d12da541afdaaa2a9fb7
dataaug <38125327+dataaug@users.noreply.github.com>
2022-03-27 23:20:31 +0800

    Update simple_agent.py

--- Building a Basic Agent/simple_agent.py
+++ Building a Basic Agent/simple_agent.py
@@ -58,7 +58,7 @@ class SimpleAgent(base_agent.BaseAgent):
             
         if not self.supply_depot_built:
             if not self.scv_selected:
-                unit_type = obs.observation["featurescreen"][_UNIT_TYPE]
+                unit_type = obs.observation["feature_screen"][_UNIT_TYPE]
                 unit_y, unit_x = (unit_type == _TERRAN_SCV).nonzero()
                 
                 target = [unit_x[0], unit_y[0]]
